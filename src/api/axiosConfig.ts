81efbbfb62e9863d5b5d4382b918687200c889eb
backend-developer-hojiakbar <soyibnazarovhoji@gmail.com>
2025-09-10 11:38:13 +0500

    addss

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://api.qxjurnal.uz/api/',
   headers: {
     'Content-Type': 'application/json',
   },
